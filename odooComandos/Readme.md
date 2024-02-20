59ce5171e3fb5643d7e1a3f65f27988d2f15126b
Lider Máximo <llopezraccioppe@danielcastelao.org>
2024-02-20 14:42:17 +0100

    con cambias y Readme.md

---
+++ odooComandos/Readme.md
@@ -0,0 +1,17 @@
+# Manejo de Odoo casi sin morir
+
+## Cambios 
+
+
+
+.
+### Punto desconocido
+
+En `openacademy/views/views.xml` se descomentan las siguietes lineas:
+- los action del modelo
+- el top menu item
+- el menu category
+- el action con el action_windows
+
+recordar los otros del mafest de habilitar el security, cambiar el nombre del modelo con el que tiene el modelo (test_model en mi caso), y luego en los actions cambiar lo del openacademy.opencademy por el nombre de la modelo
+
--- odooComandos/extra-addons/openacademy/__manifest__.py
+++ odooComandos/extra-addons/openacademy/__manifest__.py
@@ -16,17 +16,17 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'data/datos.xml'
+        'data/datos.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
